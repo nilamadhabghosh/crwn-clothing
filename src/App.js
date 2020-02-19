e54d1ac4d0da9a962f222c7d2e11008775d71987
NG075210 <ng075210@cerner.net>
2020-02-19 16:10:22 +0530

    Collection Preview and Collection Item component Added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 const HatPage = () => (
   <div>
@@ -13,7 +14,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatPage} />
+        <Route path="/shops" component={ShopPage} />
       </Switch>
     </div>
   );
