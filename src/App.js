537a1fba970df4c6d507e0669f432e6dc9db81be
NG075210 <ng075210@cerner.net>
2020-02-20 15:47:34 +0530

    Added Sign out and styling to custom buttons

--- src/App.js
+++ src/App.js
@@ -5,24 +5,47 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up-page/sign-in-and-sign-up.componant";
+import { auth } from "./firebase/firebase.utils";
 
 const Contact = () => (
   <div>
     <h1>Contact PAGE</h1>
   </div>
 );
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUpPage} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribrFromAuth = null;
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(this.state.currentUser);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribrFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
