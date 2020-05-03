e04a667c08fd15a3074c3b507934aca52e396eaa
NG075210 <ng075210@cerner.net>
2020-05-03 18:41:56 +0530

    Added State Normalization

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { selectCollections } from '../../redux/shop/shop.selector';
+import { selectCollectionsPreview } from '../../redux/shop/shop.selector';
 import CollectionPreview from '../Collection_preview/Collection_Preview.component';
 
 const CollectionOverview = ({ collections }) => (
@@ -14,7 +14,7 @@ const CollectionOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsPreview
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
